8d6b15cd3e9d1bf910ca7f97e9a404d6f68833c0
Akshay Khobragade <akshay.khobragade7875@gmail.com>
2021-12-08 23:55:53 +0530

    String Reverse alt solutions

--- stringReverse.js
+++ stringReverse.js
@@ -1,3 +1,44 @@
+// I have added performance calculation as well, so function wont normally work if you
+// run in terminal
+// please paste this code in chrome debugger to run the program
+
+// solution 1
 const stringReverse = (str) => {
   return str.split("").reverse().join("");
 };
+
+// solution 2
+const stringReverse2 = (str) => {
+  let reversed = "";
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+};
+
+// solution 3
+const stringReverse3 = (str) => {
+  return str.split("").reduce((reversed, char) => char + reversed);
+};
+
+let t1 = performance.now();
+console.log(
+  stringReverse("appleappleappleappleappleappleappleappleappleappleappleapple")
+);
+let t2 = performance.now();
+
+let t3 = performance.now();
+console.log(
+  stringReverse2("appleappleappleappleappleappleappleappleappleappleappleapple")
+);
+let t4 = performance.now();
+
+let t5 = performance.now();
+console.log(
+  stringReverse3("appleappleappleappleappleappleappleappleappleappleappleapple")
+);
+let t6 = performance.now();
+
+console.log(`performace - ${(t2 - t1) / 1000}`);
+console.log(`performace 2 - ${(t4 - t3) / 1000}`);
+console.log(`performace 3 - ${(t6 - t5) / 1000}`);
