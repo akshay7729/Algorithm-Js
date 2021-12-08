2af6a0081f822a1a97d733d1408d53425278d5cd
Akshay Khobragade <akshay.khobragade7875@gmail.com>
2021-12-09 00:03:19 +0530

    Palindrome alt solution

--- Palindrome.js
+++ Palindrome.js
@@ -1,6 +1,13 @@
-var palindrome = (str) => {
+const palindrome = (str) => {
   var original = str
     .replace(/[\s"'.,-\/#!$%\^&*;:{}=\-_`~()\\\[\]@+|?><]/g, "")
     .toLowerCase();
   return original === original.split("").reverse().join("");
+};
+
+// palindrome alt solution
+const palindromeAlt = (str) => {
+  str.split("").every((char, index) => {
+    return char === str[str.length - i - 1];
+  });
 };
